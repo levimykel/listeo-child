73c7f0fe843c69ca66ec73b40d678e4be0f4eee5
Levi Gable <levimykel@gmail.com>
2020-10-22 20:23:46 +0200

    Move the detailed itinerary button

--- listeo-core/single-partials/single-listing-itinerary.php
+++ listeo-core/single-partials/single-listing-itinerary.php
@@ -1,12 +1,21 @@
 <!-- Itinerary -->
 <?php
   $itinerary = get_post_meta($post->ID, '_itinerary', false);
+
+  $custom_fields = Listeo_Core_Meta_Boxes::meta_boxes_custom();
+  $detailed_itinerary = get_post_meta($post->ID, '_detailed_itinerary', false);
 ?>
 
 <!-- Itinerary -->
 <div id="itinerary" class="listing-section">
 	<h3 class="listing-desc-headline margin-top-60 margin-bottom-30"><?php esc_html_e('Itinerary','listeo_core'); ?></h3>
   <?php echo wpautop($itinerary[0]); ?>
+
+  <?php if(!empty($detailed_itinerary) && !empty($detailed_itinerary[0])): ?>
+    <div class="margin-top-30" style="text-align: center;">
+      <a href=<?php echo $detailed_itinerary[0] ?> class="button" target="_blank">Download detailed itinerary & FAQ</a>
+    </div>
+  <?php endif; ?>
 
   <?php 
     // echo "<pre>";
