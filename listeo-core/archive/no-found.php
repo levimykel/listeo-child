968dcc2399ac75c84acac5ee8aafe5e90ceed9eb
Levi Gable <levimykel@gmail.com>
2020-12-02 22:50:43 +0100

    Update listings not found text

---
+++ listeo-core/archive/no-found.php
@@ -0,0 +1,14 @@
+<div id="listeo-listings-container">
+  <div class="loader-ajax-container" style="">
+    <div class="loader-ajax"></div>
+  </div>
+  <section id="listings-not-found" class="margin-bottom-50 col-md-12">
+    <h2><?php esc_html_e('Bummer! We didn&rsquo;t find anything.','listeo_core'); ?></h2>
+    <p>
+      <?php _e( 'We&rsquo;re regularly adding more trips to the site, so check back soon.', 'listeo_core' ); ?>
+    </p>
+    <p>
+      <?php _e( 'You can also try the "Browse All" page under our Destinations tab, or refine your search using another keyword such as "cultural", "trek", "wine", "country", etc.', 'listeo_core' ); ?>
+    </p>
+  </section>
+</div>
