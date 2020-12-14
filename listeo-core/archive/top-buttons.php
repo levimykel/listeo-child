537c55bf66ca28a773dc30e2610b939b135193db
Levi Gable <levimykel@gmail.com>
2020-12-14 19:19:33 +0100

    Update listings sorting options

--- listeo-core/archive/top-buttons.php
+++ listeo-core/archive/top-buttons.php
@@ -108,14 +108,9 @@ $segments = explode('|',$buttons);
 		<div class="sort-by-select">
 			<?php $default = isset( $_GET['listeo_core_order'] ) ? (string) $_GET['listeo_core_order']  :  get_option( 'listeo_sort_by','date' );  ?>
 			<select form="listeo_core-search-form" name="listeo_core_order" data-placeholder="<?php esc_attr_e('Default order', 'listeo_core'); ?>" class="chosen-select-no-single orderby" >
-				<option <?php selected($default,'default'); ?> value="default"><?php esc_html_e( 'Default Order' , 'listeo_core' ); ?></option>	
-				<option <?php selected($default,'highest-rated'); ?> value="highest-rated"><?php esc_html_e( 'Highest Rated' , 'listeo_core' ); ?></option>
-				<option <?php selected($default,'reviewed'); ?> value="reviewed"><?php esc_html_e( 'Most Reviewed' , 'listeo_core' ); ?></option>
 				<option <?php selected($default,'date-desc'); ?> value="date-desc"><?php esc_html_e( 'Newest Listings' , 'listeo_core' ); ?></option>
 				<option <?php selected($default,'date-asc'); ?> value="date-asc"><?php esc_html_e( 'Oldest Listings' , 'listeo_core' ); ?></option>
 				<option <?php selected($default,'featured'); ?> value="featured"><?php esc_html_e( 'Featured' , 'listeo_core' ); ?></option>
-				<option <?php selected($default,'views'); ?> value="views"><?php esc_html_e( 'Most Views' , 'listeo_core' ); ?></option>
-				<option <?php selected($default,'rand'); ?> value="rand"><?php esc_html_e( 'Random' , 'listeo_core' ); ?></option>
 			</select>
 		</div>
 	</div>
