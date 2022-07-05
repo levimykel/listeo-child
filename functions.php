d868af844fde69d0fb383537785e041714e39052
Levi Gable <levimykel@gmail.com>
2022-07-05 20:28:39 +0200

    fix: update shop styles

--- functions.php
+++ functions.php
@@ -12,6 +12,17 @@ function remove_parent_theme_features() {
 }
 add_action( 'after_setup_theme', 'remove_parent_theme_features', 10 );
 
+add_filter('loop_shop_columns', 'loop_columns', 999);
+if (!function_exists('loop_columns')) {
+    function loop_columns() {
+        return 3; // 3 products per row
+    }
+}
 
+add_action('woocommerce_before_shop_loop', 'show_category_title', 10, 2);
+function show_category_title() {
+	$cat_title = single_tag_title("", false);
+	echo '<h1>' . $cat_title . '</h1>';
+}
 
 ?>
